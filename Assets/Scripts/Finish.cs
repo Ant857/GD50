730f58545858f027e3fdf0bf88b5e3e3c0b77bfb
Ant857 <Ant857@users.noreply.github.com>
2020-12-02 17:05:34 -0500

    automated commit by submit50

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Finish : MonoBehaviour
+{
+    public Text winText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Finish")
+        {
+            winText.gameObject.SetActive(true);
+        }
+    }
+}
