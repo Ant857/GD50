a9a2cfb379c7c7930f23ff37a6ca2f5aba4b0a6b
Ant857 <Ant857@users.noreply.github.com>
2020-12-02 14:58:36 -0500

    automated commit by submit50

---
+++ Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DespawnOnHeight : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        //check if player has fallen in a hole
+        if (transform.position.y <= -1)
+        {
+            Destroy(GameObject.Find("WhisperSource"));
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static int level = 1;
+    public Text levelText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelText = GameObject.Find("levelText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        levelText.text = "Level: " + level;
+    }
+}
